3189dd1e7fb0d118bee7eafaeaf77b6051429a3d
Byron Gomez Jr <byrongomezjr@protonmail.com>
2022-04-29 02:33:09 -0400

    deleted old code.

--- components/CustomListItem.js
+++ components/CustomListItem.js
@@ -1,32 +1,3 @@
-// import React from 'react';
-// import {StyleSheet, Text, View} from 'react-native';
-// import {ListItem, Avatar} from 'react-native-elements';
-
-// const CustomListItem = (id, chatName, enterChat) => {
-//   return (
-//     <ListItem>
-//       <Avatar
-//         rounded
-//         source={{
-//           uri: 'https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png',
-//         }}
-//       />
-
-//       <ListItem.Content>
-//         <ListItem.Title style={{fontWeight: '700'}}>Byron Gomez</ListItem.Title>
-//         <ListItem.Subtitle numberOfLines={1} ellipsizeMode="tail">
-//           This is a test subtitle, this is a test subtitle, this is a test
-//           subtitle, this is a test subtitle
-//         </ListItem.Subtitle>
-//       </ListItem.Content>
-//     </ListItem>
-//   );
-// };
-
-// export default CustomListItem;
-
-// const styles = StyleSheet.create({});
-
 import React from 'react';
 import { StyleSheet, Text, View, TouchableOpacity, } from "react-native";
 import {Avatar} from 'react-native-elements';
